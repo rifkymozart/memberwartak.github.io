0a3fda73011ddf15ab838c36c47aa89f76a8dedf
rifkymozart <rifkymozart2@gmail.com>
2025-01-07 14:34:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-document.getElementById('form-yatim').addEventListener('submit', function(e) {
+document.getElementById('form-yatim').addEventListener('submit', function (e) {
   e.preventDefault();
 
-  const nama = document.getElementById('nama').value.toLowerCase();
+  const nama = document.getElementById('nama').value.trim().toLowerCase();
   const hasil = document.getElementById('hasil');
 
   const yatim = ['dedy', 'dedi', 'catur'];
   const yatimPasif = 'apriyan';
-  const memberWartak = ['dicky', 'subani', 'bani', 'rifky', 'rifki', 'iqro', 'ilham', 'fakih']
+  const memberWartak = ['dicky', 'subani', 'bani', 'rifky', 'rifki', 'iqro', 'ilham', 'fakih'];
 
-  if (nama.includes(yatimPasif)) {
-    hasil.innerText = 'Kamu Yatim Pasif!';
+  if (nama === '') {
+    hasil.innerHTML = '<span style="color: red;">Nama tidak boleh kosong!</span>';
+  } else if (nama.includes(yatimPasif)) {
+    hasil.innerHTML = '<span style="color: orange;">Kamu Yatim Pasif!</span>';
   } else if (yatim.some(kata => nama.includes(kata))) {
-    hasil.innerText = 'Kamu Yatim!';
+    hasil.innerHTML = '<span style="color: green;">Kamu Yatim!</span>';
   } else if (memberWartak.some(kata => nama.includes(kata))) {
-    hasil.innerText = 'Kamu Bukan Yatim!';
+    hasil.innerHTML = '<span style="color: blue;">Kamu Bukan Yatim!</span>';
   } else {
-     hasil.innerText = 'Daftar Member Wartak Dulu AJG!';
+    hasil.innerHTML = '<span style="color: red;">Daftar Member Wartak Dulu AJG!</span>';
   }
 });
