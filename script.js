2fe3e4c956190b9d058c6b98a116cfabb90d3bd6
rifkymozart <rifkymozart2@gmail.com>
2025-01-07 14:04:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-const form = document.getElementById('form-yatim');
-const hasil = document.getElementById('hasil');
-
-form.addEventListener('submit', function(e) {
+document.getElementById('form-yatim').addEventListener('submit', function(e) {
   e.preventDefault();
 
   const nama = document.getElementById('nama').value.toLowerCase();
+  const hasil = document.getElementById('hasil');
+
   const yatim = ['dedy', 'dedi', 'catur'];
+  const yatimPasif = 'apriyan';
+  const memberWartak = ['dicky', 'subani', 'bani', 'rifky', 'rifki', 'iqro', 'ilham', 'fakih']
 
-  let ketemu = false;
-  yatim.forEach(function(kata) {
-    if (nama.includes(kata)) {
-      ketemu = true;
-    }
-  });
-
-  if (ketemu) {
-    hasil.innerText = 'Kamu Yatim';
+  if (nama.includes(yatimPasif)) {
+    hasil.innerText = 'Kamu Yatim Pasif!';
+  } else if (yatim.some(kata => nama.includes(kata))) {
+    hasil.innerText = 'Kamu Yatim!';
+  } else if (memberWartak.some(kata => nama.includes(kata))) {
+    hasil.innerText = 'Kamu Bukan Yatim!';
   } else {
-    hasil.innerText = 'Kamu Bukan Yatim';
+     hasil.innerText = 'Daftar Member Wartak Dulu AJG!';
   }
 });
