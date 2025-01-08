589727e0e772687ccace36e92869a1a43eca20b1
rifkymozart <rifkymozart2@gmail.com>
2025-01-08 11:04:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ document.getElementById('form-yatim').addEventListener('submit', function (e) {
   const nama = document.getElementById('nama').value.trim().toLowerCase();
   const hasil = document.getElementById('hasil');
 
-  const yatim = ['dedy', 'dedi', 'catur'];
-  const yatimPasif = 'apriyan';
-  const memberWartak = ['dicky', 'subani', 'bani', 'rifky', 'rifki', 'iqro', 'ilham', 'fakih'];
+  const memberAzzahra = ['dicky', 'subani', 'bani', 'rifky', 'rifki', 'iqro', 'ilham', 'fakih', 'wahyu', 'abdul', 'muid', 'anjas', 'catur', 'dedy', 'dedi', 'apriyan', 'dicki'];
+  const khodam = ['anjing', 'babi', 'kucing', 'beruk', 'tapir', 'srengenge', 'tuyul', 'macan', 'singa', 'jokowi', 'cobra', 'ayam', 'sapi', 'wewe gombel', 'cicak', 'monyet', 'naga', 'beruang', 'doraemon', 'spongebob', 'patrick'];
+  const ciwi = ['zakia', 'imah', 'imalatul'];
+  const khodamCiwi = ['marmut', 'merak', 'angsa', 'kucing', 'hamster', 'bebek', 'barbie', 'capung', 'peri', 'bidadari'];
+
+  const randomKhodam = khodam[Math.floor(Math.random() * khodam.length)].toUpperCase();
+  const randomKhodamCiwi = khodamCiwi[Math.floor(Math.random() * khodamCiwi.length)].toUpperCase();
 
   if (nama === '') {
     hasil.innerHTML = '<span style="color: red;">Nama tidak boleh kosong!</span>';
-  } else if (nama.includes(yatimPasif)) {
-    hasil.innerHTML = '<span style="color: orange;">Kamu Yatim Pasif!</span>';
-  } else if (yatim.some(kata => nama.includes(kata))) {
-    hasil.innerHTML = '<span style="color: green;">Kamu Yatim!</span>';
-  } else if (memberWartak.some(kata => nama.includes(kata))) {
-    hasil.innerHTML = '<span style="color: blue;">Kamu Bukan Yatim!</span>';
+  } else if (memberAzzahra.some(kata => nama.includes(kata))) {
+    hasil.innerHTML = `<span style="color: blue;">Khodam kamu adalah (${randomKhodam})</span>`;
+  } else if (ciwi.some(kata => nama.includes(kata))) {
+    hasil.innerHTML = `<span style="color: blue;">Khodam kamu adalah (${randomKhodamCiwi})</span>`;
   } else {
-    hasil.innerHTML = '<span style="color: red;">Daftar Member Wartak Dulu AJG!</span>';
+    hasil.innerHTML = '<span style="color: red;">Daftar Member Az Zahra dulu ya...</span>';
   }
 });
